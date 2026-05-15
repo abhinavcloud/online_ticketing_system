1b34a6be80d929d8850cf42ff140d3afb0221325
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-05-15 11:02:07 +0530

    Intialised terraform and github actions workflows

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -1,11 +1,11 @@
-output "availilbility_zones" {
+output "availilbility_zones_id" {
     value = data.aws_availability_zones.az.id
 }
 
-output "availilbility_zones" {
+output "availilbility_zones_zoneids" {
     value = data.aws_availability_zones.az.zone_ids
 }
 
-output "availilbility_zones" {
+output "availilbility_zones_names" {
     value = data.aws_availability_zones.az.names
 }
