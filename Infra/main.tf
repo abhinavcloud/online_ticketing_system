bf02ad89c0f5edaceef8dfdcd48d57c6c8bef2e8
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-05-16 22:46:50 +0530

    updates to database tf module

--- Infra/main.tf
+++ Infra/main.tf
@@ -19,5 +19,5 @@ module "Database" {
 
     source = "./Database/"
     availability_zones   = data.aws_availability_zones.az.names
-    subnet_group = [var.subnet_01, var.subnet_02, var.subnet_03]
+    subnet_group = [moudle.Network.subnet_01, module.Network.subnet_02, module.Network.subnet_03]
 }
