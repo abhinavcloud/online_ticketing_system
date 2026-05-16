cedd31943b4abcde6a43e8867b5f696ea21c32fb
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-05-16 09:29:41 +0530

    Updates to network tf module

--- Infra/Network/main.tf
+++ Infra/Network/main.tf
@@ -22,8 +22,7 @@ resource "aws_subnet" "private_subnets" {
   for_each = var.private_subnets # using for_each to create 3 subnets for each item in the private subnet list variable
   vpc_id              = aws_vpc.vpc.id
   cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
-  availability_zone   = data.aws_availability_zones.available.names[0]
-  depends_on = [aws_vpc.vpc]
+  availability_zone   = data.aws_availability_zones.available.names[each.value]
     
   tags = merge(
     var.common_tags,
@@ -37,7 +36,7 @@ resource "aws_subnet" "private_subnets" {
 
 # Route Tables
 resource "aws_route_table" "private_route_table" {
-  for_each = aws_subnet.private_subnets
+  for_each = var.private_subnets
   vpc_id = aws_vpc.vpc.id
 
   tags = merge(
@@ -48,4 +47,11 @@ resource "aws_route_table" "private_route_table" {
     AvailabilityZones = var.az_name_tag
     } 
   )
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = var.private_subnets
+  route_table_id = aws_route_table.private_route_table[each.key].id
+  subnet_id      = aws_subnet.private_subnets[each.key].id
+  
 }
