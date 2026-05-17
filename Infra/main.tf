1b6f5e1c6ecdf6b95005a4a2123219e17e016953
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-05-17 13:21:46 +0530

    updates to database modules

--- Infra/main.tf
+++ Infra/main.tf
@@ -20,7 +20,7 @@ module "Database" {
     source = "./Database/"
     availability_zones   = data.aws_availability_zones.az.names
     subnet_group = [module.Network.subnet_01, module.Network.subnet_02, module.Network.subnet_03]
-    vpc_id = var.vpc_id
+    vpc_id = module.Network.vpc_id
     region = data.aws_region.current.id
     account_id = var.account_id
     master_username = var.master_username
