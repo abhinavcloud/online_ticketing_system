95d4c85a427feece1b0fd0aaea437ad7f56763fb
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-05-17 12:48:48 +0530

    updates to database modules

--- Infra/Database/main.tf
+++ Infra/Database/main.tf
@@ -155,9 +155,9 @@ resource "aws_security_group" "rds_proxy_sg" {
 #resource "aws_vpc_security_group_ingress_rule" "rds_proxy_sg_ingress_rule" {
 #  security_group_id = aws_security_group.rds_proxy_sg.id
 #  referenced_security_group_id = aws_security_group.lambda_sg.id
-#  from_port         = 80
+#  from_port         = 5432
 #  ip_protocol       = "tcp"
-#  to_port           = 80
+#  to_port           = 5432
 #}
 
 # Creating an Egress rule from RDS Proxy security group to Aurora security group
