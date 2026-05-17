2ad67b54c534038a3e743bb717755ab4a2deffad
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-05-17 13:25:40 +0530

    updates to database modules

--- Infra/Database/main.tf
+++ Infra/Database/main.tf
@@ -37,7 +37,7 @@ resource "aws_rds_cluster" "online-ticketing-system" {
   engine_version     = "17.7"
   database_name      = "online-ticketing-system"
   master_username    = var.master_username
-  master_password    = var.master_passwrod
+  master_password    = var.master_password
   storage_encrypted  = true
   db_subnet_group_name = aws_db_subnet_group.aurora_db_subnet_group.name
   enable_http_endpoint = false
@@ -90,7 +90,7 @@ resource "aws_rds_cluster_instance" "reader_instance_02" {
 
 # Create a policy to attach to a IAM role for RDS Proxy to connect to Aurora DB via IAM Authentication
 resource "aws_iam_policy" "rds_proxy_allow_aurora_db_connection" {
-  name        = "rds_proxy_allow_aurora_db_connection"
+  name        = "rds-proxy-allow-aurora-db-connection"
   path        = "/"
   description = "This is an Allow Policy for RDS Proxy to allow to connect to Aurora DB Cluster since we will be using end to end IAM authentication for DB connections"
 
@@ -126,7 +126,7 @@ data "aws_iam_policy_document" "rds_proxy_assume_role" {
 
 # Create a RDS Proxy role with the Assume Role Policy identifying the principal who can assume this role.
 resource "aws_iam_role" "rds_proxy_role" {
-  name               = "test-role"
+  name               = "rds-proxy-role"
   assume_role_policy = data.aws_iam_policy_document.rds_proxy_assume_role.json
 }
 
@@ -140,7 +140,7 @@ resource "aws_iam_role_policy_attachment" "attach_rds_proxy_allow_aurora_db_conn
 
 # Create a security group for RDS Proxy to Allow Egress to Aurora DB Cluster and Ingress from Lambda
 resource "aws_security_group" "rds_proxy_sg" {
-  name        = "rds_proxy_sg"
+  name        = "rds-proxy-sg"
   description = "Allow TLS inbound traffic from lambda and outbound traffic to aurora cluster"
   vpc_id      = var.vpc_id
 
@@ -163,8 +163,7 @@ resource "aws_security_group" "rds_proxy_sg" {
 # Creating an Egress rule from RDS Proxy security group to Aurora security group
 resource "aws_vpc_security_group_egress_rule" "rds_proxy_sg_egress_rule" {
   security_group_id = aws_security_group.rds_proxy_sg.id
-
- referenced_security_group_id  = aws_security_group.aurora_sg.id
+  referenced_security_group_id  = aws_security_group.aurora_sg.id
   from_port   = 5432
   ip_protocol = "tcp"
   to_port     = 5432
@@ -172,7 +171,7 @@ resource "aws_vpc_security_group_egress_rule" "rds_proxy_sg_egress_rule" {
 
 # Create a RDS Proxy with end to end IAM Authentication so that db credentials are not needed in Secrets Manager
 resource "aws_db_proxy" "rds_proxy" {
-  name                   = "rds_proxy"
+  name                   = "rds-proxy"
   debug_logging          = false
   engine_family          = "POSTGRESQL"
   idle_client_timeout    = 1200
