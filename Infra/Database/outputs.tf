4ce4b25671d9d219244f484b47604b8fd950689e
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-05-18 14:18:35 +0530

    Finalised the cache module

--- Infra/Database/outputs.tf
+++ Infra/Database/outputs.tf
@@ -5,3 +5,15 @@ output "db_proxy_arn" {
 output "db_proxy_security_group" {
     value = aws_security_group.rds_proxy_sg.id
 }
+
+output "db_cluster_endpoint" {
+  value = aws_rds_cluster.online-ticketing-system.endpoint
+}
+
+output "db_cluster_port" {
+  value = aws_rds_cluster.online-ticketing-system.port
+}
+
+output "db_name" {
+  value = aws_rds_cluster.online-ticketing-system.database_name
+}
---
+++ Infra/outputs.tf
@@ -0,0 +1,11 @@
+output "db_cluster_endpoint" {
+  value = module.Database.db_cluster_endpoint
+}
+
+output "db_cluster_port" {
+  value = module.Database.db_cluster_port
+}
+
+output "db_name" {
+  value = module.Database.db_name
+}
