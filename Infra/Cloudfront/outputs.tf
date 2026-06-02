0f251589a667d12aa134afecd80034bf7f23ff06
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-06-02 20:57:45 +0530

    fixed cloudfront resources

--- Infra/Cloudfront/outputs.tf
+++ Infra/Cloudfront/outputs.tf
@@ -1,5 +1,5 @@
 output "cloudfront_domain_name" {
-  value       = aws_cloudfront_distribution.site.domain_name
+  value       = aws_cloudfront_distribution.site_ticketing.domain_name
   description = "CloudFront domain name"
 }
 
