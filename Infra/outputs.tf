b00448ba9d95f9d3d9a041388b88fc58e13bbd3d
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-06-02 11:56:47 +0530

    updated code

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -37,10 +37,7 @@ output "cloudfront_domain_name" {
   description = "CloudFront domain name"
 }
 
-output "distribution_id" {
-  value       = module.Cloudfront.distribution_id
-  description = "CloudFront distribution id (use in CI for invalidations)"
-}
+
 
 output "acm_dns_validation" {
   value = module.Certificate.acm_dns_validation
