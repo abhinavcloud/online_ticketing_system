eb95dde2d4f99fdc86010ff3f9816e364eb5b093
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-06-12 23:22:43 +0530

    adding transit encyption to true in caches

--- Infra/Cache/main.tf
+++ Infra/Cache/main.tf
@@ -97,6 +97,7 @@ resource "aws_elasticache_replication_group" "browse_cache" {
   snapshot_retention_limit   = 1
   apply_immediately          = true
   auto_minor_version_upgrade = true
+  user_group_ids = aws_elasticache_user_group.elasticache_user_group.id
 
  tags = {
    Name = "browse-cache-cache"
@@ -148,7 +149,7 @@ resource "aws_elasticache_replication_group" "active_users" {
 
   subnet_group_name  = aws_elasticache_subnet_group.elasticache_subnet.name
   security_group_ids = [aws_security_group.elasticache_sg.id]
-
+  user_group_ids = aws_elasticache_user_group.elasticache_user_group.id
   parameter_group_name       = "default.valkey9"
   snapshot_retention_limit   = 1
   apply_immediately          = true
@@ -205,7 +206,7 @@ resource "aws_elasticache_replication_group" "seat_lock" {
 
   subnet_group_name  = aws_elasticache_subnet_group.elasticache_subnet.name
   security_group_ids = [aws_security_group.elasticache_sg.id]
-
+  user_group_ids = aws_elasticache_user_group.elasticache_user_group.id
   parameter_group_name       = "default.valkey9"
   snapshot_retention_limit   = 1
   apply_immediately          = true
